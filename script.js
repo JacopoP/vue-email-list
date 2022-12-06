02c8fd135588a2700a9372ddc5a39ff585977f29
Jac22 <jacopo.prudentino@gmail.com>
2022-12-06 17:00:26 +0100

    rese visualizzabili mail dopo generazione di tutte

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ createApp({
     data(){
         return{
             emailList : [],
+            nEmail : 10,
+            ready : false,
         }
     },
     mounted(){
-        for(let i=0; i<10; i++){
+        for(let i=0; i<this.nEmail; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((result)=>{
                 this.emailList.push(result.data.response);
-            })
+            });
+            if(i + 1 === this.nEmail){
+                this.ready = true;
+            }
         }
     }
 }).mount("#app");
